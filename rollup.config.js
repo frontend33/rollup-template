10fec07d805d8e5c5b680eaa5cf3ffc2692876ed
georgy.vikulov <georgy.vikulov@aliexpress.ru>
2021-03-04 11:56:48 +0300

    added rollup config

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default [{
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.min.js', format: 'iife' }],
+    plugins: [
+        // todo: попозже накидаем сюда плагинов
+    ],
+}];
