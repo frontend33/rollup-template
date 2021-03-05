dd138b84728a70941098bc90234897700bc05944
georgy.vikulov <georgy.vikulov@aliexpress.ru>
2021-03-05 20:32:41 +0300

    fix config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonJs from 'rollup-plugin-commonjs';
-// import typeScript from 'rollup-plugin-typescript2';
+import typeScript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import html from 'rollup-plugin-html';
 import visualizer from 'rollup-plugin-visualizer';
@@ -9,21 +9,67 @@ import {terser} from 'rollup-plugin-terser';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-
-export default [{
-    input: 'src/index.ts',
-    output: [{ file: 'dist/index.r.min.js', format: 'iife' }],
-    plugins: [
+const getPlugins = (options) => [
         nodeResolve({
             mainFields: ['module', 'main', 'jsnext:main', 'browser'],
             extensions
         }), // подключение модулей node
-        commonJs(), // подключение модулей commonjs
-        postcss(), // подключение препроцессора postcc, а также стилей scss и less
-        html(), // подключение html файлов
-        // typeScript({tsconfig: "tsconfig.json"}), // подключение typescript
-        sizeSnapshot(), // напишет в консоль размер бандла
-        terser(), // минификатор совместимый с ES2015+, форк и наследник UglifyES
-        visualizer() // анализатор бандла,
-    ]
-}];
+    commonJs(),
+    postcss(),
+    html(),
+    typeScript({
+        tsconfig: "tsconfig.json",
+        tsconfigOverride: { compilerOptions: { "target": options.target } }
+    }),
+    sizeSnapshot(),
+    terser(),
+    visualizer()
+];
+
+export default [
+    // {
+    // input: 'src/polyfills.ts',
+    // output: [{ file: 'dist/polyfills.min.js', format: 'iife' }],
+    // plugins: getPlugins({ target: "es5" })
+    // },
+    {
+        input: 'src/index.ts',
+        output: [{ file: 'dist/index.next.min.js', format: 'iife' }],
+        plugins: getPlugins({ target: "esnext" })
+    },{
+        input: 'src/index.ts',
+        output: [{ file: 'dist/index.es5.min.js', format: 'iife' }],
+        plugins: getPlugins({ target: "es5" })
+    },{
+        input: 'src/index.ts',
+        output: [{ file: 'dist/index.es3.min.js', format: 'iife' }],
+        plugins: getPlugins({ target: "es3" })
+    },
+    // {
+    //     input: 'src/serviceworker.ts',
+    //     output: [{ file: 'dist/serviceworker.min.js', format: 'iife' }],
+    //     plugins: getPlugins({ target: "es5" })
+    // },{
+    //     input: 'src/webworker.ts',
+    //     output: [{ file: 'dist/webworker.min.js', format: 'iife' }],
+    //     plugins: getPlugins({ target: "es5" })
+    // }
+];
+
+// export default [{
+//     input: 'src/index.ts',
+//     output: [{ file: 'dist/index.r.min.js', format: 'iife' }],
+//     plugins: [
+//         nodeResolve({
+//             mainFields: ['module', 'main', 'jsnext:main', 'browser'],
+//             extensions
+//         }), // подключение модулей node
+//         commonJs(), // подключение модулей commonjs
+//         postcss(), // подключение препроцессора postcc, а также стилей scss и less
+//         html(), // подключение html файлов
+//         // typeScript({tsconfig: "tsconfig.json"}), // подключение typescript
+//         sizeSnapshot(), // напишет в консоль размер бандла
+//         terser(), // минификатор совместимый с ES2015+, форк и наследник UglifyES
+//         visualizer() // анализатор бандла,
+//     ]
+// }];
