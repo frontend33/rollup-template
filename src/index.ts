6c43708da03a37de93ea55f0f0eeddaa201c2e34
georgy.vikulov <georgy.vikulov@aliexpress.ru>
2021-03-13 21:06:29 +0300

    fixed config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 // index.ts
-import {TestB } from './mymodule'
+import { TestB } from './mymodule'
 
 const test = new TestB()
 console.log('Hello, world!', test.getData())
 
-// var a = 5
-// console.log('a', a)
-//
-//
-// var foo = 1;
-// console.log(foo);
-// var bar;
-// var = 1;
-// var baz = 123;
+var a = 5
+console.log('a', a)
+
+
+var foo = 1;
+console.log(foo);
+var bar;
+var = 1;
+var baz = 123;
