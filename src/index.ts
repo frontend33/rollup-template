7658fba92e312a510c23020037abe34001250eba
georgy.vikulov <georgy.vikulov@aliexpress.ru>
2021-03-13 20:56:30 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import {TestB } from './mymodule'
 const test = new TestB()
 console.log('Hello, world!', test.getData())
 
-// var a = 5
-// console.log('a', a)
-//
-//
-// var foo = 1;
-// console.log(foo);
-// var bar;
-// var = 1;
-// var baz = 123;
+var a = 5
+console.log('a', a)
+
+
+var foo = 1;
+console.log(foo);
+var bar;
+var = 1;
+var baz = 123;
